d927424d61f394f70ffe5eb1b81271ac2b8984b1
kopi21144 <kopi21144@gmail.com>
2026-06-12 03:57:02 +0200

    Update main.sol

--- main.sol
+++ main.sol
@@ -1034,3 +1034,77 @@ contract SoriatMixer {
         depositor = n.depositor;
         tier = n.blendTier;
         locked = n.lockedWei;
+        digest = keccak256(abi.encode(noteId, locked, _PEPPER_4));
+    }
+
+    function peekNote_21(bytes32 noteId) external view returns (
+        uint256 potId,
+        address depositor,
+        uint8 tier,
+        uint256 locked,
+        bytes32 digest
+    ) {
+        SrmNote storage n = notes[noteId];
+        potId = n.potId;
+        depositor = n.depositor;
+        tier = n.blendTier;
+        locked = n.lockedWei;
+        digest = keccak256(abi.encode(noteId, locked, _PEPPER_5));
+    }
+
+    function peekNote_22(bytes32 noteId) external view returns (
+        uint256 potId,
+        address depositor,
+        uint8 tier,
+        uint256 locked,
+        bytes32 digest
+    ) {
+        SrmNote storage n = notes[noteId];
+        potId = n.potId;
+        depositor = n.depositor;
+        tier = n.blendTier;
+        locked = n.lockedWei;
+        digest = keccak256(abi.encode(noteId, locked, _PEPPER_6));
+    }
+
+    function peekNote_23(bytes32 noteId) external view returns (
+        uint256 potId,
+        address depositor,
+        uint8 tier,
+        uint256 locked,
+        bytes32 digest
+    ) {
+        SrmNote storage n = notes[noteId];
+        potId = n.potId;
+        depositor = n.depositor;
+        tier = n.blendTier;
+        locked = n.lockedWei;
+        digest = keccak256(abi.encode(noteId, locked, _PEPPER_7));
+    }
+
+    function peekNote_24(bytes32 noteId) external view returns (
+        uint256 potId,
+        address depositor,
+        uint8 tier,
+        uint256 locked,
+        bytes32 digest
+    ) {
+        SrmNote storage n = notes[noteId];
+        potId = n.potId;
+        depositor = n.depositor;
+        tier = n.blendTier;
+        locked = n.lockedWei;
+        digest = keccak256(abi.encode(noteId, locked, _PEPPER_0));
+    }
+
+    function peekNote_25(bytes32 noteId) external view returns (
+        uint256 potId,
+        address depositor,
+        uint8 tier,
+        uint256 locked,
+        bytes32 digest
+    ) {
+        SrmNote storage n = notes[noteId];
+        potId = n.potId;
+        depositor = n.depositor;
+        tier = n.blendTier;
